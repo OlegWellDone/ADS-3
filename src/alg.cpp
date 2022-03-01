f87f363aaaa0955c52afc5d59d7cbe12dd0d48da
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-03-01 21:28:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,34 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int i = 0;
+  int j = size - 1;
+  int k = 0;
+  int mid = (i + (j - 1)) / 2;
+  while (arr[j] > value) {
+    mid = (i + (j - 1)) / 2;
+    if (arr[mid] == value) {
+      k = k + 1;
+      j = mid;
+      break;
+    }
+    else if (arr[mid] > value){
+      j = mid;
+    }
+    i = 1;
+    while ((arr[j + i] == value) || (arr[j - i] == value)) {
+      if (arr[j + i] == value) {
+        k = k + 1;
+      }
+      if (arr[j - i] == value) {
+        k = k + 1;
+      }
+      i = i + 1;
+    }
+  }
+  if (k != 0) {
+    return k;
+  } else {
+  return 0;
+  }
 }
