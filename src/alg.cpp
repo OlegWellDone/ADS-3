2caaad7399166f9c558d32ac9ac996622171ea3e
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2022-03-03 23:46:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,16 +15,15 @@ int cbinsearch(int *arr, int size, int value) {
      j = mid;
     }
     i = 1;
-    while (((arr[j + i] == value) && ((j + 1) <= size)) || ((arr[j - i] == value) && ((j - i) >= 0))) {
-      if (arr[j + i] == value) {
-        k = k + 1;
-      }
-      if (arr[j - i] == value) {
-        k = k + 1;
-      }
+    while ((arr[j + i] == value) && ((j + 1)<= size))  {
+      k = k + 1;
       i = i + 1;
-    }
-  }
+      }
+    i = 1;
+      while ((arr[j - i] == value) && ((j - 1)<= size)) {
+      k = k + 1;
+      i = i + 1;
+      }
   if (k != 0) {
     return k;
   } else {
